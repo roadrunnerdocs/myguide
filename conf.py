be8169bee58a5e2f986eacdbb439894cb5892406
roadrunnerdocs <contact@boughtiolo.com>
2025-06-16 14:54:12 +0530

    Add files via upload

---
+++ conf.py
@@ -0,0 +1,13 @@
+# docs/conf.py
+
+project = 'RoadRunner Docs'
+author = 'Your Name'
+release = '1.0'
+
+extensions = ['sphinx.ext.autodoc']
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
